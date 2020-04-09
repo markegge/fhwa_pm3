06b04d9686f4b63d197732e97ac4fc51e76f2082
Mark Egge <mark@eateggs.com>
2020-04-09 11:40:25 -0600

    updated readme

--- R/pm3/R/pm3.R
+++ R/pm3/R/pm3.R
@@ -1,7 +1,13 @@
-#' PM3 Tools: A package for computating the notorious bar statistic.
+#' PM3 Tools: A package for calculating PM3 Travel Time Reliability Scores from NPMRDS Data
 #'
-#' The foo package provides three categories of important functions:
-#' foo, bar and baz.
+#' This package will help download the correct subset of NPMRDS data, calculate
+#' segment-level LOTTR and TTTR metric scores for segments, and aggregate 
+#' segment level scores to statewide measures.
+#' 
+#' Note: if your state has a large amount of data, you may encounter
+#' a "Error: vector memory exhausted (limit reached?)" error. See this
+#' StackOverflow post on resolving: 
+#' https://stackoverflow.com/questions/51295402/r-on-macos-error-vector-memory-exhausted-limit-reached
 #' 
 #' @section Foo functions:
 #' The foo functions ...
---
+++ R/pm3/README.md
@@ -0,0 +1,10 @@
+# PM3 Performance Score Calculation Instructions
+
+1. Download the shapefiles for the desired year(s) from (https://npmrds.ritis.org/analytics/shapefiles)[https://npmrds.ritis.org/analytics/shapefiles]
+
+## Installation
+
+```r
+require(devtools)
+install_bitbucket("high-street/pm3/R/pm3")
+```
